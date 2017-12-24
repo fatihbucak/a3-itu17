34fc7ce3ddaee968e6d4be1569f50160eb587dad
fatihbucak <32177116+fatihbucak@users.noreply.github.com>
2017-12-25 02:38:17 +0300

    added filtering

--- bottle_app.py
+++ bottle_app.py
@@ -12,7 +12,6 @@ contents = []
 input_file = open("a2_input.csv","r")
 for row in reader(input_file):
     contents = contents + [row]
-
 def htmlify(title,text):
     page = """
     <!DOCTYPE html>
@@ -23,26 +22,36 @@ def htmlify(title,text):
             <link rel="stylesheet" href="/static/custom.css"/>
         </head>
         <body>
-            %s
+            <div id="main">
+                <a id="logo" href="/"><img src="static/home.png" alt="Time Travel"/></a>
+                <h1 id="title">Movies About Time Travel</h1>
+                %s
+            </div>
         </body>
     </html>
-    """ % (title,text)
+    """ % (title, text)
     return page
 
 def sort_html_code():
     form = '''
-    <form action="/sort" method="POST">
+    <form id="form" action="/sort_search" method="POST">
         <table>
             <tr>
-                <td>
+                <td class="odd">
+                    <span>Search</span></br>
+                    <input type="text" name="search" value="%s" placeholder = "Movie"/>
+                </td>
+            
+                <td class="even">
                     <div>
-                        <input type="radio" name="desc_or_asc" value="descending" %s/>Descending<br/>
-                        <input type="radio" name="desc_or_asc" value="ascending" %s/>Ascending<br/>
+                        <input type="radio" name="desc_or_asc" value="descending" %s/>Descending order<br/>
+                        <input type="radio" name="desc_or_asc" value="ascending" %s/>Ascending order<br/>
                     </div>
                 </td>
-                <td>
+            
+                <td class="odd">
                     <div>
-                        <span>sort by</span></br>
+                        <span>Sort by</span></br>
                         <select name="column_no">
                             <option value="1" %s>Year</option>
                             <option value="2" %s>IMDb</option>
@@ -50,18 +59,33 @@ def sort_html_code():
                         </select>
                     </div>
                 </td>
-                <td>
-                    <input type="text" name="search" value="%s"/>
-                </td>
-                <td>
-                
-                    <span>search in</span></br>
-                    <input type="checkbox" name="search_type" value="0">Year<br>
-                    <input type="checkbox" name="search_type" value="1">IMDb (greater than)<br>
-                    <input type="checkbox" name="search_type" value="2">Duration
-                </td>
+            
+                <td class="even">
+                    <span>Filter</span></br>
                     
-                <td>
+                    <input type="checkbox" name="filter_no" value="1" %s/>Year
+                    <select name="year_filter_type">
+                        <option value="0" %s>&gt;</option>
+                        <option value="1" %s>&lt;</option>
+                    </select>
+                    <input class="numbers" type="number" name="year_filter_text" value="%s"/><br/>
+                    
+                    <input type="checkbox" name="filter_no" value="2" %s/>IMDb
+                    <select name="imdb_filter_type">
+                        <option value="0" %s>&gt;</option>
+                        <option value="1" %s>&lt;</option>
+                    </select>
+                    <input class="numbers" type="number" name="imdb_filter_text" value="%s"/><br/>
+                    
+                    <input type="checkbox" name="filter_no" value="3" %s/>Duration
+                    <select name="duration_filter_type">
+                        <option value="0" %s>&gt;</option>
+                        <option value="1" %s>&lt;</option>
+                    </select>
+                    <input class="numbers" type="number" name="duration_filter_text" value="%s"/><br/>
+                </td>
+            
+                <td class="odd">
                     <input type="submit" value="Search and Sort"/><br/>
                 </td>
             </tr>
@@ -70,29 +94,53 @@ def sort_html_code():
     '''
     return form
 
-def comment_form_html_code(content):
-    form = '''
-    <form action="/save_comment/%s" method="POST">
-        <input type="text" name="comment_text" value=""/><br/>
-        <input type="submit" value="Save"/>
-    </form>
-    ''' % content
-    return form
 
-def comment_button(movie_name):
-    code= '''<form action="/comments/%s" method="POST">
-        <input type="submit" value="Comment"/>
-    </form>
-    ''' % movie_name
-    return code
-def sort(list, desc_or_asc, column_no, search):
+def sort_search(list, desc_or_asc, column_no, search, filter_no, year_filter_type, imdb_filter_type, duration_filter_type, year_filter_text,imdb_filter_text, duration_filter_text):
 
     ordered_list = []
 
     for row in list:
-        if search.lower() in str(row).lower():
+        if search.lower() in str(row[0]).lower():
             ordered_list += [row]
 
+    
+    if "1" in filter_no:
+        temporary_list = ordered_list
+        ordered_list = []
+        if year_filter_type == "0":
+            for row in temporary_list:
+                if int(row[1]) > int(year_filter_text):
+                    ordered_list += [row]
+        elif year_filter_type == "1":
+            for row in temporary_list:
+                if int(row[1]) < int(year_filter_text):
+                    ordered_list += [row]
+
+    
+    if "2" in filter_no:
+        temporary_list = ordered_list
+        ordered_list = []
+        if imdb_filter_type == "0":
+            for row in temporary_list:
+                if float(row[2]) > float(imdb_filter_text):
+                    ordered_list += [row]
+        elif imdb_filter_type == "1":
+            for row in temporary_list:
+                if float(row[2]) < float(imdb_filter_text):
+                    ordered_list += [row]
+                    
+    if "3" in filter_no:
+        temporary_list = ordered_list
+        ordered_list = []
+        if duration_filter_type == "0":
+            for row in temporary_list:
+                if int(row[3]) > int(duration_filter_text):
+                    ordered_list += [row]
+        elif duration_filter_type == "1":
+            for row in temporary_list:
+                if int(row[3]) < int(duration_filter_text):
+                    ordered_list += [row]
+    
     if desc_or_asc == "ascending":
         while True:
             all_is_well = True
@@ -119,68 +167,100 @@ def sort(list, desc_or_asc, column_no, search):
 
 def get_table_html_code(list):
     if len(list) == 0:
-        return "<h1>Not Found!\nPlease try again</h1>"
+        return "<h4>Not Found!\nPlease try again</h4>"
     table_code = '''
-    <tr>
-        <th>Movies About Time Travel</th>
-        <th>Year</th>
-        <th>IMDb</th>
-        <th>Duration</th>
-        <th>Comment</th>
+    <tr class="tr_list">
+        <th class="list_th">Movies</th>
+        <th class="list_th">Year</th>
+        <th class="list_th">IMDb</th>
+        <th class="list_th">Duration</th>
     </tr>
     '''
     for rows in range(len(list)):
         row = ""
         for column in range(len(list[rows])):
-            row += "<td>%s</td>\n" % str(list[rows][column])
-        row += "<td>%s</td>\n" % comment_button(list[rows][0])
-        table_code += "<tr>\n%s\n</tr>\n" % row
-    return "<table>\n%s\n</table>" % table_code
+            row += "<td class=\"list_td\">%s</td>\n" % str(list[rows][column])
+        table_code += "<tr class=\"tr_list\">\n%s\n</tr>\n" % row
+    return '''<table id="list_table" >\n%s\n</table>''' % table_code
 
 def index():
     return htmlify("Time Travel Movies",
-                   sort_html_code() % ("checked","","selected","","","")+get_table_html_code(contents))
+                   sort_html_code() % ("","checked","","selected","","","","selected","","2000","","selected","","7","","selected","","90") + get_table_html_code(contents))
 
 def order_page():
     desc_or_asc = request.POST['desc_or_asc']
     column_no = request.POST['column_no']
     search = request.POST['search']
-    sort_html = sort_html_code() % ("%s","%s","%s","%s","%s",search)
+    filter_no = request.POST.getall('filter_no')
+    year_filter_type = request.POST['year_filter_type']
+    imdb_filter_type = request.POST['imdb_filter_type']
+    duration_filter_type = request.POST['duration_filter_type']
+    year_filter_text = request.POST['year_filter_text']
+    imdb_filter_text = request.POST['imdb_filter_text']
+    duration_filter_text = request.POST['duration_filter_text']
+    
+    
     if desc_or_asc == "descending":
-        sort_html = sort_html % ("checked","","%s","%s","%s")
+        desc="checked"
+        asce=""
     elif desc_or_asc == "ascending": 
-        sort_html = sort_html % ("","checked","%s","%s","%s")
+        desc=""
+        asce="checked"
+
+    col1=""
+    col2=""
+    col3=""
 
     if column_no == "1":
-        sort_html = sort_html % ("selected","","")
+        col1="selected"
+
     elif column_no == "2":
-        sort_html = sort_html % ("","selected","")
+        col2="selected"
+
     elif column_no == "3":
-        sort_html = sort_html % ("","","selected")
+        col3="selected"
+        
+    if "1" in filter_no:
+        check1 = "checked"
+    else:
+        check1 = ""
+
+    if "2" in filter_no:
+        check2 = "checked"
+    else:
+        check2 = ""
+
+    if "3" in filter_no:
+        check3 = "checked"
+    else:
+        check3 = ""
+
+    yft0 = ""
+    yft1 = ""
+    if year_filter_type == "0":
+        yft0 = "selected"
+    elif year_filter_type == "1":
+        yft1 = "selected"
+        
+    ift0 = ""
+    ift1 = ""
+    if imdb_filter_type == "0":
+        ift0 = "selected"
+    elif imdb_filter_type == "1":
+        ift1 = "selected"
+
+
+    dft0 = ""
+    dft1 = ""
+    if duration_filter_type == "0":
+        dft0 = "selected"
+    elif duration_filter_type == "1":
+        dft1 = "selected"
+        
+    sort_html = sort_html_code() % (search,desc,asce,col1,col2,col3,check1,yft0,yft1,year_filter_text,check2,ift0,ift1,imdb_filter_text,check3,dft0,dft1,duration_filter_text)
 
     return htmlify("Time Travel Movies",
-                   sort_html+get_table_html_code(sort(contents, desc_or_asc, int(column_no),search)))
-
-def comment_page(movie_name):
-    return htmlify("Comment",
-                   comment_form_html_code(movie_name))
-
-def show_comments():
-    comments = []
-    comments_file = open("comments.txt","r")
-    for row in reader(comments_file):
-        comments = comments + [row]
-    return str(comments)
-    
-
-def save_comment(content):
-    comment_text = request.POST['comment_text']
-    comments_file = open('comments.txt','w')
-    comments_file.write(comment_text+ " " + content)
-    comments_file.close()
-    return htmlify("Comment",
-                   "kaydedildi: " + comment_text+ " " + content)
-
+                   sort_html + get_table_html_code(sort_search(contents, desc_or_asc, int(column_no),search, filter_no, year_filter_type, imdb_filter_type, duration_filter_type, year_filter_text,imdb_filter_text, duration_filter_text)))
 
 
 def server_static(path):
@@ -188,10 +268,7 @@ def server_static(path):
 
 
 route('/', 'GET', index)
-route('/sort', 'POST', order_page)
-route('/comments/<movie_name>', 'POST', comment_page)
-route('/save_comment/<content>', 'POST', save_comment)
-route('/comments', 'GET', show_comments)
+route('/sort_search', 'POST', order_page)
 route('/static/<path>', 'GET', server_static)
 
 #####################################################################
